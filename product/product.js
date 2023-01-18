616270c54f3c21ae6bbaaf83ce7ce24c440e6b6b
sandy shinde <samirshinde011@gmail.com>
2023-01-18 15:56:19 +0530

    commiting for first day

---
+++ product/product.js
@@ -0,0 +1,276 @@
+
+// let BaseUrl = "https://images.bewakoof.com/t640/"
+
+
+const url ="https://63c4d1cdf80fabd877db7506.mockapi.io/newproduct";
+
+/*all the event listner are here*/
+
+
+let container = document.getElementById("container")
+
+let filter_container = document.getElementById("filter_container")
+let sort_container= document.getElementById("sort_Size_Container")
+
+
+
+
+
+
+let data_For_Other_application= []
+/*  event listner end */
+window.addEventListener("load",showdata)
+
+function showdata(){
+    // console.log("Hi")
+    fetchValut()
+    displayfilter()
+    displaySort()
+
+   
+}
+
+function fetchValut(){
+
+fetch(url)
+.then((res)=> res.json())
+.then((data)=>{
+    console.log(data)
+    let needyData = data.filter((el)=>{
+        console.log(+el.id>=61)
+        if(+el.id>=71 && +el.id!=92){
+            return el
+        }
+    })
+    // console.log(needyData)
+    rendercard(needyData)
+    
+ 
+    data_For_Other_application=needyData
+})
+}
+
+
+function cart(){
+
+  let bag = document.getElementById("cursor")
+  bag.addEventListener("click",()=>{
+    console.log("HI")
+  })
+}
+
+
+function filter_giver(){
+
+  let for_filter= document.getElementById("filter_Size")
+  
+  for_filter.addEventListener("click",()=>{
+    // console.log("Hi")
+    console.log(for_filter.value)
+    let filtered_Data=data_For_Other_application.filter(el=>{
+      if(for_filter.value==el.Size){
+        return el
+      }else if(for_filter.value==el.Size){
+        return el
+      }else if(for_filter.value==el.Size){
+        return el
+      }else if(for_filter.value==""){
+        return el
+      }
+    })
+    // console.log(filtered_Data)
+    if(filtered_Data.length==0){
+      rendercard(needyData)
+   
+
+    }else{
+      rendercard(filtered_Data)
+    
+    }
+  
+  })
+}
+
+
+
+
+function filter_BY_colour(){
+
+  let filter_By_Colour= document.getElementById("filter_Colour")
+  filter_By_Colour.addEventListener("click",()=>{
+  
+    let filtered_Data_By_Colour= data_For_Other_application.filter(el=>{
+      if(el.colour_for_sorting== filter_By_Colour.value){
+        return el
+      }else if(el.colour_for_sorting == filter_By_Colour.value){
+        return el
+      }else if(el.colour_for_sorting== filter_By_Colour.value){
+        return el
+      }else if(filter_By_Colour.value == ""){
+        return el
+      }
+    })
+    if(filtered_Data_By_Colour.length==0){
+      rendercard(needyData)
+
+    }else{
+      rendercard(filtered_Data_By_Colour)
+    }
+    // console.log(filtered_Data_By_Colour)
+  })
+
+}
+
+function sortByPrice(){
+  let sort_price = document.getElementById("sort_price")
+
+  sort_price.addEventListener("click",()=>{
+    
+    let sortedData= data_For_Other_application.sort((a,b)=>{
+      if(sort_price.value === "L_T_H"){
+        return (+a.price) - (+b.price)
+      }else if(sort_price.value === "H_T_L"){
+        return (+b.price)- (+a.price)
+      }
+      
+      
+    })
+
+
+    if(sortedData.length == 0){
+      rendercard(needyData)
+    }else{
+      rendercard( sortedData)
+    }
+    console.log(sortedData)
+  })
+
+}
+
+/*        */
+
+
+
+
+/*  */
+
+
+
+
+
+
+
+
+
+
+
+
+function displayfilter(){
+  filter_container.innerHTML=createfilter()
+  
+  filter_giver()
+  filter_BY_colour()
+
+}
+
+function displaySort(){
+  sort_container.innerHTML=createSort()
+  sortByPrice()
+}
+
+
+
+
+/* All the utilities ie card making , filter maker, sort maker */
+
+
+function rendercard(data){
+  let cardData=[]
+data.forEach(item=>{
+  let card=`
+
+    
+
+  <div class="product">
+  <img src=${item.img} alt="Product Image" id="product-image">
+  
+  <div id="product_info">
+    <div id="actual_product">
+    <h3 id="product-name">${item.name}</h3>
+    <p id="product-price">RS. ${item.price}</p>
+    
+
+  </div>
+    <div class="Add_to_cart_box">
+      <img src="https://www.shutterstock.com/image-vector/shopping-bag-icon-flat-vector-600w-1670113852.jpg" id="Add_to_cart" id="cursor"   >
+    </div>
+  
+  </div>
+  
+</div>
+  
+  `
+  cardData.push(card) 
+})
+
+container.innerHTML=cardData.join("")
+
+
+}
+
+
+
+function createfilter(){
+
+  let filterDiv=`
+
+  <div class="filter_Size_Container" id="filter_Size_Container">
+
+  <label for="filter_Size">filter By Size:</label>
+  <select name="for_Sort" id="filter_Size">
+    <option value="">Select</option>
+    <option value="XL">XL</option>
+    <option value="M">Medium</option>
+    <option value="S">Small</option>
+    <option value="L">Larg</option>
+  </select>
+
+</div>
+  
+<div class="filter_Colour_Container" id="filter_Colour_Container">
+   
+        
+  <label for="filter_Colour">filter By Colour:</label>
+  <select name="for_Colour" id="filter_Colour">
+    <option value="">Select</option>
+    <option value="Green">Green</option>
+    <option value="White">White</option>
+    <option value="Black">Black</option>
+  </select>
+
+
+</div>
+  
+  `
+
+  return filterDiv
+}
+
+
+
+
+function createSort(){
+
+
+  let sortDiv=`
+  <label for="sort_price">sort By Price:</label>
+      <select name="for_Sort" id="sort_price">
+        <option value="">Select</option>
+        <option value="L_T_H">Price Low To High</option>
+        <option value="H_T_L">Price High To Low</option>
+       
+      </select>
+  `
+
+  return sortDiv
+}
